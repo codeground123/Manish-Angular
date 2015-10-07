e0cace6d5b6f5c8f630a9ef0d0cb4bf1524956ff
unknown <phaniwithu@gmail.com>
2015-10-07 09:02:28 +0530

    using emit broadcast and tests

---
+++ js/controllers/childController.js
@@ -0,0 +1,17 @@
+﻿myApp.controller("childController", function ($scope, $rootScope) {
+
+
+    $scope.recRootMessage = $rootScope.rootMessage;
+
+    $scope.message = "I am child controller";
+    $scope.newMessage = "";
+    $scope.updateParent = function () {
+        $scope.childMessage = "updating parent from child controller";
+        $scope.$emit("update_parent_controller", $scope.childMessage);
+    };
+
+    $scope.$on("parent_message_changed", function (evt, message) {
+        $scope.newMessage = message;
+
+    });
+});
